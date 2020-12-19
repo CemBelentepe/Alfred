6956b293fad68ad952a597eb8db4d71328813cad
fatihyigit29 <fatihyigit29@hotmail.com>
2020-12-19 20:09:58 +0300

    FrontEnd Added

--- AlfredApp/Form1.cs
+++ AlfredApp/Form1.cs
@@ -7,31 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using AlfredESK;
 
-namespace AlfredApp
+namespace ProjeArayuz1
 {
     public partial class Form1 : Form
     {
-        Alfred esk;
         public Form1()
         {
-            esk = new Alfred();
             InitializeComponent();
-            boxLocations.SelectedIndex = 0;
         }
 
-        private void btnSearch_Click(object sender, EventArgs e)
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            var results = esk.Search(new SearchInfo(
-                boxLocations.SelectedItem.ToString(),
-                int.Parse(tbxAreaMin.Text),
-                int.Parse(tbxAreaMin.Text)));
-            foreach (var result in results)
-            {
-                lbxResults.Items.Add(result);
-            }
-            // test
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Nasıl.BackColor = ButNasıl.BackColor;
+            Evoz.BackColor = panel1.BackColor;
+            Eks.BackColor = panel1.BackColor;
+            kon.BackColor = panel1.BackColor;
+            panelny.BringToFront();
+            
+        }
+
+        private void ButKon_Click(object sender, EventArgs e)
+        {
+            kon.BackColor = ButKon.BackColor;
+            Evoz.BackColor = panel1.BackColor;
+            Eks.BackColor = panel1.BackColor;
+            Nasıl.BackColor = panel1.BackColor;
+            panelkb.BringToFront();
+            
+        }
+
+        private void ButEvoz_Click(object sender, EventArgs e)
+        {
+            Evoz.BackColor = ButEvoz.BackColor;
+            kon.BackColor = panel1.BackColor;
+            Eks.BackColor = panel1.BackColor;
+            Nasıl.BackColor = panel1.BackColor;
+            paneleo.BringToFront();
+            
+        }
+
+        private void ButEks_Click(object sender, EventArgs e)
+        {
+            Eks.BackColor = ButEks.BackColor;
+            Evoz.BackColor = panel1.BackColor;
+            kon.BackColor = panel1.BackColor;
+            Nasıl.BackColor = panel1.BackColor;
+            panelam.BringToFront();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panelsonuc.BringToFront();
         }
     }
 }
